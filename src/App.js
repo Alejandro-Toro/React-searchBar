f84c34876e7f9ae19d77c9e00ff906fb95207a4f
Alejandro Toro <alejandroctk@gmail.com>
2023-03-15 23:48:42 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { BaseColaboradores } from "./components/BaseColaboradores";
+
+
 
 function App() {
+  const [colaboradores, setColaboradores] = useState(BaseColaboradores)
+  const [nuevoColaborador, setNuevoColaborador] = useState({
+    id: "",
+    nombre: "",
+    correo: "",
+  });
+
+  const [busqueda, setNuevaBusqueda] = useState("");
+
+  const agregarColaborador = (e) => {
+    e.preventDefault();
+
+    if (nuevoColaborador.nombre === "" || nuevoColaborador.correo === ""){
+          return alert("Faltan campos por llenar")
+    }
+
+    setColaboradores([...colaboradores, nuevoColaborador]);
+
+    setNuevoColaborador({
+      id: "",
+      nombre: "",
+      correo: "",
+    });
+  };
+
+  const colaboradoresFiltrados = colaboradores.filter((colaborador) => {
+    if (colaborador.nombre.toLowerCase().includes(busqueda.toLowerCase())) {
+      return true
+      
+    }
+    return false
+
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <nav>
+      <input 
+      className="form-control me-2 my-3"
+      placeholder="Buscar un colaborador"
+      onChange={(e) => setNuevaBusqueda(e.target.value)}
+      value={busqueda}/>
+    </nav>
+
+    <form action="" onSubmit={agregarColaborador} className="Formulario"> 
+      <h3>Agrega un colaborador</h3>
+      <hr/>
+      <label>Nombre colaborador</label>
+      
+      <input 
+      className="form-control me-2 my-3"
+      type="text"
+      onChange={(e) => setNuevoColaborador({
+        id: Date.now(),
+        nombre: e.target.value,
+        correo: nuevoColaborador.correo,
+      })}
+      value={nuevoColaborador.nombre}
+      />
+
+      <label>Correo del colaborador</label>
+
+      <input 
+      className="form-control me-2 my-3"
+      type="email"
+      onChange={(e) => setNuevoColaborador({
+        id: Date.now(),
+        nombre: nuevoColaborador.nombre,
+        correo: e.target.value,
+      })}
+      value={nuevoColaborador.correo}
+      />
+
+      <button
+      className="mt-0 btn btn-primary"
+      type="submit"
+      >Agregar colaborador</button>
+
+    </form>
+    <hr/>
+    <div className="Lista">
+      
+      <h3>Lista Colaboradores</h3>
+
+        <ul>
+          {colaboradoresFiltrados.map( ({id, nombre, correo}) => <li key={id}>{nombre} | {correo}</li>)}
+        </ul>
     </div>
+    </>
   );
 }
 
